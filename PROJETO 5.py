18de0fdd377724c310f6b9e0634920880c112b17
Sidney Santos <132826593+sidneysantosdatascientist@users.noreply.github.com>
2024-02-01 12:44:19 -0300

    Add files via upload

---
+++ PROJETO 5.py
@@ -0,0 +1,23 @@
+import os
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title="Selecione uma pasta")
+#print(caminho)
+lista_arquivos = os.listdir(caminho)
+#print(lista_arquivos)
+
+locais = {
+    "imagens": [".png", ".jpg"],
+    "planilhas": [".xlsx"],
+    "pdfs": [".pdf"],
+    "csv": [".csv"],    
+}
+
+for arquivo in lista_arquivos:
+    nome , extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        #print(pasta)
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")    
